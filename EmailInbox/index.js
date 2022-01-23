a4417b4cf9125e0d67f9550d3d56f08609d26318
abhay05 <abhaypsc.5@gmail.com>
2022-01-23 21:38:36 +0530

    swipe flatlist

---
+++ EmailInbox/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import EmailInbox from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => EmailInbox);
